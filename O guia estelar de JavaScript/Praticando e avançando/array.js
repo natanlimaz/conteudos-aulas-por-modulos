d61ce68dbb83f52fba7c2c0f9b370bb0bfbe3e26
natanlimaz <natanlimadev@gmail.com>
2022-01-24 13:39:11 -0300

    master

---
+++ O guia estelar de JavaScript/Praticando e avançando/array.js
@@ -0,0 +1,13 @@
+// Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Natan',
+        age: 20
+    }
+]
+
+// Acessar valores dentro do Array
+console.log(animals[2].name)
---
+++ O guia estelar de JavaScript/Praticando e avançando/object.js
@@ -0,0 +1,10 @@
+// Object
+
+const person = {
+    name: 'Jhon',
+    age: 30, 
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(`${person.name} tem ${person.age} anos.`)
