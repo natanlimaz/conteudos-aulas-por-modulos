1b95035053d4207ef59470eb729ee4011b38bca0
natanlimaz <natanlimadev@gmail.com>
2022-01-24 20:12:29 -0300

    master

---
+++ O guia estelar de JavaScript/Funções/functionConstructor.js
@@ -0,0 +1,23 @@
+/*
+    Function() constructor
+
+    * expressão new
+    * Criar um novo objeto
+    * this keyword
+*/
+
+/*
+function Person(name) {
+    this.name = name
+    this.walk = function(){
+        return this.name + " está andando"
+    }
+}
+const natan = new Person("Natanael") // Instânciar um novo objeto
+const joao = new Person("João")
+console.log(natan.walk())
+console.log(joao.walk())
+*/
+
+let date = new Date(2020-12-01)
+console.log(date)
