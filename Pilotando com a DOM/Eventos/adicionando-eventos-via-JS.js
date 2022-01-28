4a027f3f2a9c6f1d0234bad8e154a09d0ca1af31
natanlimaz <natanlimadev@gmail.com>
2022-01-28 02:08:20 -0300

    master

---
+++ Pilotando com a DOM/Eventos/adicionando-eventos-via-JS.js
@@ -0,0 +1,9 @@
+// Eventos
+
+const h1 = document.querySelector('h1')
+
+h1.addEventListener('', print) //('tipo do evento', 'funçao que quero executar')
+
+function print() {
+    console.log('print')
+}
